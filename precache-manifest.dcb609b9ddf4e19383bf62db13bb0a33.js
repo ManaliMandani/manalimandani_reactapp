f070c3ab02eddde74d407f6bbe16b793044d9308
ManaliMandani <manalimandani1997@gmail.com>
2020-03-13 12:13:41 +0530

    Updates

---
+++ precache-manifest.dcb609b9ddf4e19383bf62db13bb0a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452b6b134088659f304dced670d4cfed",
+    "url": "/manalimandani_reactapp/index.html"
+  },
+  {
+    "revision": "691fb3538561c56705a3",
+    "url": "/manalimandani_reactapp/static/css/main.fa74953d.chunk.css"
+  },
+  {
+    "revision": "896b07372e0a8a20e91e",
+    "url": "/manalimandani_reactapp/static/js/2.03805ceb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/manalimandani_reactapp/static/js/2.03805ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691fb3538561c56705a3",
+    "url": "/manalimandani_reactapp/static/js/main.4ac944b8.chunk.js"
+  },
+  {
+    "revision": "fcaa637abdea80e79f8c",
+    "url": "/manalimandani_reactapp/static/js/runtime-main.5ede4a71.js"
+  }
+]);
